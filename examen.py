d35aa8a85f244d9bef4137cf85add4309e635efa
moiselon <moisesmendezm.2001@gmail.com>
2021-02-19 21:34:05 -0400

    Create examen.py
    MOISES-MENDEZ-2020-9360

---
+++ examen.py
@@ -0,0 +1,220 @@
+print("--------------------------------------")
+print("--------------------------------------")
+print("Bienvenido al banco FDP INVERSMENTS\n")
+print("--------------------------------------")
+print("--------------------------------------\n")
+
+monto = int(input("Cuanto desea retirar: "))
+
+if monto < 10000:
+    print("El monto minimo por retiro es de 10,000 pesos")
+elif monto > 20000:
+    print("El monto maximo por retiro es de 20,000 pesos")
+elif monto== 10000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil")
+elif monto == 10100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil y un billete de 100")
+elif monto == 10200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil y un billetes de 200")
+elif monto == 10300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil, un billete de 100 y un billete se 200")
+elif monto ==10400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil y dos billetes de 200")
+elif monto == 10500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil y un billete de 500")
+elif monto == 10600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil,un billete de 500 y un billete de 100")
+elif monto== 10700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 10800:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil, un billete de 500, dos billetes de 200 y un billete de 100")
+elif monto == 10900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 10 billetes de mil, un billete de 500, dos billete de 200 y un billete de 100")
+elif monto == 11000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil")
+elif monto == 11100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil y un billete de 100")
+elif monto == 11200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil y un billete de 200")
+elif monto == 11300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, un billetes de 200 y un billete de 100")
+elif monto == 11400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil y dos billetes de 200")
+elif monto == 11500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, dos billetes de 200 y un billete de 100")
+elif monto == 11600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 11700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 11800:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 11900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 11 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto== 12000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil")
+elif monto == 12100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y un billete de 100")
+elif monto == 12200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y un billete de 200")
+elif monto == 12300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 12400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y dos billetes de 200")
+elif monto == 12500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y un billetes de 500")
+elif monto == 12600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil,  billetes de 500")
+elif monto == 12700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y siete billetes de 500")
+elif monto== 12800:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y ocho billetes de 500")
+elif monto == 12900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 12 billetes de mil y nueve billetes de 500")
+elif monto== 13000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil")
+elif monto == 13100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil y un billete de 100")
+elif monto ==13200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil y dos billetes de 100")
+elif monto == 13300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 13400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil y dos billetes de 200")
+elif monto == 13500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil y un billete de 500")
+elif monto == 13600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 13700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 13800:
+    print("Usted a decidido retirar",monto, "pesos que equivalen a 13 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 13900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 13 billetes de mil, un billete de 500 y dos biilletes de 200")
+elif monto == 14000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil")
+elif monto == 14100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil y un billete de 100")
+elif monto == 14200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil y un billete de 200")
+elif monto == 14300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 14400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil y dos billetes de 200")
+elif monto == 14500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil y un billete de 500")
+elif monto == 14600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 14700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 14800:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 14900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 14 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 15000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil")
+elif monto == 15100:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil y un billete de 100")
+elif monto == 15200:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil y un billete de 200")
+elif monto == 15300:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 15400:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil y dos billetes de 200")
+elif monto == 15500:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil y un billete de 500")
+elif monto == 15600:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 15700:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 15800:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 15900:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 15 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 16000:
+    print("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil,")
+elif monto == 16100:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil y un billete de 100")
+elif monto == 16200:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil y un billete de 200")
+elif monto == 16300:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 16400:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil y dos billetes de 200")
+elif monto == 16500:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil y un billete de 500")
+elif monto == 16600:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 16700:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 16800:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 16900:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 16 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 17000:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil")
+elif monto == 17100:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil y un billete de 100")
+elif monto == 17200:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil y un billete de 200")
+elif monto == 17300:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 17400:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil y dos billetes de 200")
+elif monto == 17500:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil y un billete de 500")
+elif monto == 17600:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 17700:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 17800:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 17900:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 17 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 18000:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil")
+elif monto == 18100:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil y un billete de 100")
+elif monto == 18200:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil y un billete de 200")
+elif monto == 18300:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 18400:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil y dos billetes de 200")
+elif monto == 18500:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil y un billete de 500")
+elif monto == 18600:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 18700:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 18800:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 18900:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 18 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 19000:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil")
+elif monto == 19100:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil y un billete de 100")
+elif monto == 19200:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil y un billete de 200")
+elif monto == 19300:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil, un billete de 200 y un billete de 100")
+elif monto == 19400:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil y dos billetes de 200")
+elif monto == 19500:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil y un billete de 500")
+elif monto == 19600:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil, un billete de 500 y un billete de 100")
+elif monto == 19700:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil, un billete de 500 y un billete de 200")
+elif monto == 19800:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil, un billete de 500, un billete de 200 y un billete de 100")
+elif monto == 19900:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 19 billetes de mil, un billete de 500 y dos billetes de 200")
+elif monto == 20000:
+    print ("Usted a decidido retirar", monto, "pesos que equivalen a 20 billetes de mil")
+
+print("--------------------------------------")
+print("--------------------------------------")
+print ("fin.")
+print("--------------------------------------")
+print("--------------------------------------")
